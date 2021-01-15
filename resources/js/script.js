8e875edfc0acd80e591ee83f74ed9559fab54a40
Moha9166 <64616836+Moha9166@users.noreply.github.com>
2021-01-15 14:46:33 +0100

    d

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,12 +1,13 @@
+//Asks the user to introduce a color to asignate
 nombre=prompt("Write a color in english")
 
-
+//Reads and asigns a value to the .css variable
 let root = document.documentElement;
+// asigns the introduced color to the title color
 root.style.setProperty('--hola', ""+nombre)
+// Makes posible the movement of the custom mouse cursor
 root.addEventListener("mousemove", e => {
   root.style.setProperty('--mouse-x', e.clientX + "px");
   root.style.setProperty('--mouse-y', e.clientY + "px");
   
 });
-
-
