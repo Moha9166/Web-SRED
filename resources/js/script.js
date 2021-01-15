bc1a52640a48400f020c26ecd2fb35ca0a892444
Moha9166 <64616836+Moha9166@users.noreply.github.com>
2021-01-15 14:20:02 +0100

    Update script.js

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,21 +1,12 @@
-nombre=prompt("dime un color")
-azul="azul"
+nombre=prompt("Write a color in english")
 
-if (nombre == azul){
-  let root = document.documentElement;
-  root.style.setProperty('--hola', "blue")
-}
-else{
-    alert("red")
-    let root = document.documentElement;
-    root.style.setProperty('--hola', "red")
-}
 
 let root = document.documentElement;
-
+root.style.setProperty('--hola', ""+nombre)
 root.addEventListener("mousemove", e => {
   root.style.setProperty('--mouse-x', e.clientX + "px");
   root.style.setProperty('--mouse-y', e.clientY + "px");
+  
 });
 
 
